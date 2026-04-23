253fe4818817d555f1938c8c8b0e5d6a2b5f4a76
SAI CHUNCHU <122979651+saichunchu@users.noreply.github.com>
2026-04-23 12:03:49 +0530

    Add: minimum-path-sum - submission-2

---
+++ Data Structures & Algorithms/minimum-path-sum/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+        int[][] dp = new int[n][m];
+        //Tabulation method
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0){
+                    dp[i][j]= grid[0][0];
+                }else {
+                    int up=Integer.MAX_VALUE;
+                    int left=Integer.MAX_VALUE;
+                    if(i>0){
+                        up=grid[i][j]+dp[i-1][j];
+                    }
+                    if(j>0){
+                        left=grid[i][j]+dp[i][j-1];
+                    }
+                    dp[i][j]=Math.min(up,left);
+                }
+            }
+        }
+        return dp[n-1][m-1];
+        
+        
+    }
+   
+    }
